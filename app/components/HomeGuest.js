fd36f8cb4fde82dde1331fee39d144b2b04dfec3
Jason McKelvey <jason.mckelvey@companycam.com>
2020-06-16 10:38:30 -0500

    setup initial form value state

--- app/components/HomeGuest.js
+++ app/components/HomeGuest.js
@@ -1,19 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Page from "./Page";
 import Axios from "axios";
 
 function HomeGuest() {
+  const [username, setUsername] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
   async function handleSubmit(e) {
     e.preventDefault();
     try {
       await Axios.post("http://localhost:8080/register", {
-        username: "test2",
-        email: "test2@test.com",
-        password: "qwerty123456",
+        username,
+        email,
+        password,
       });
       console.log("User was succesfully created");
     } catch (e) {
-      console.log("there was an error.");
+      console.log(e.response.data);
     }
   }
 
@@ -36,6 +40,7 @@ function HomeGuest() {
                 <small>Username</small>
               </label>
               <input
+                onChange={(e) => setUsername(e.target.value)}
                 id="username-register"
                 name="username"
                 className="form-control"
@@ -49,6 +54,7 @@ function HomeGuest() {
                 <small>Email</small>
               </label>
               <input
+                onChange={(e) => setEmail(e.target.value)}
                 id="email-register"
                 name="email"
                 className="form-control"
@@ -62,6 +68,7 @@ function HomeGuest() {
                 <small>Password</small>
               </label>
               <input
+                onChange={(e) => setPassword(e.target.value)}
                 id="password-register"
                 name="password"
                 className="form-control"
