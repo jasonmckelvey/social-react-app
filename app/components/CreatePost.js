d08ff317677d13ae0216a0aaff37f98f68e38383
Jason McKelvey <jason.mckelvey@companycam.com>
2020-06-17 08:56:26 -0500

    Add create post routing and success message

--- app/components/CreatePost.js
+++ app/components/CreatePost.js
@@ -1,19 +1,23 @@
 import React, { useEffect, useState } from "react";
 import Page from "./Page";
 import Axios from "axios";
+import { withRouter } from "react-router-dom";
 
-function CreatePost() {
+function CreatePost(props) {
   const [title, setTitle] = useState();
   const [body, setBody] = useState();
 
   async function handleSubmit(e) {
     e.preventDefault();
     try {
-      await Axios.post("/create-post", {
+      const response = await Axios.post("/create-post", {
         title,
         body,
         token: localStorage.getItem("socialAppToken"),
       });
+      // Redirect to new post url
+      props.addFlashMessage("Congrats bro");
+      props.history.push(`/post/${response.data}`);
       console.log("new post");
     } catch {
       console.log("problem");
@@ -58,4 +62,4 @@ function CreatePost() {
   );
 }
 
-export default CreatePost;
+export default withRouter(CreatePost);
